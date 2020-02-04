4e7c24946fef04de4b8e44a2d0ab5a48270cd7ac
Robert Shearman <robert.shearman@att.com>
2020-02-04 16:31:34 +0000

    yang: add breakout-reserved interface to cstore for Apollo NCP1-1 platform
    Specify the breakout-reserved option to vyatta-interfaces.pl when
    breaking out interfaces on the NCP1-1 platform to indicate which
    interface has been reserved.
    
    This allows the dataplane to take appropriate action, such as cleaning
    up feature state to help out the FAL plugin. Whilst the mapping is
    static on this platform, it allows the breakout-reserved interface to
    be configured dynamically on platforms that support it.
    
    Fixes: VRVDR-49850

--- tmplscripts/interfaces/dataplane/breakout-reserved-for/configd_allowed-ufi-apollo-ncp1-1.sh
+++ tmplscripts/interfaces/dataplane/breakout-reserved-for/configd_allowed-ufi-apollo-ncp1-1.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 
+source /opt/vyatta/share/vyatta-cfg-dataplane/ufi-apollo-ncp1-1.functions
+
 # For unexpected interface, just return
 if ! echo $1 | grep -q dp0ce; then
     exit 0
 fi
 
-port=$(echo $1 | sed s/dp0ce//)
-if [ $port -ge 20 ]; then
-    if [ $(( $port % 2 )) = 1 ]; then
-	target_port=$(( $port - 21 ))
-    else
-	target_port=$(( $port - 19 ))
-    fi
-else
-    if [ $(( $port % 2 )) = 1 ]; then
-	target_port=$(( $port + 19 ))
-    else
-	target_port=$(( $port + 21 ))
-    fi
-fi
-echo dp0ce$target_port
+# The function is an involution, so this also works for getting the
+# breakout port for a reserved port
+reserved_port_for_breakout_port "$1"
