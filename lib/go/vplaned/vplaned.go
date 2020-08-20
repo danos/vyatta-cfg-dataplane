665b37cc698237877a4fdd5b42127d0c34260721
Nachiketa Prachanda <nprachan@vyatta.att-mail.com>
2020-08-19 20:32:40 -0700

    libgo: add dataplane command api in go
    Add apis to fetch the list of dataplanes, connect to Dataplane
    sockets and run protobuf console commands.
    
    VRVDR-48649

--- lib/go/vplaned/vplaned.go
+++ lib/go/vplaned/vplaned.go
@@ -20,6 +20,7 @@ import (
 const (
 	storeEndpoint  = "ipc:///var/run/vyatta/vplaned.socket"
 	configEndpoint = "ipc:///var/run/vyatta/vplaned-config.socket"
+	defaultDataplaneEndpoint = "ipc:///var/run/vplane.socket"
 )
 
 type Error string
@@ -32,6 +33,9 @@ const (
 	ErrNoControllerResponse = Error("No response from controller")
 	ErrConfigStoreFailed = Error("Config store failed")
 	ErrNoCommitAction = Error("COMMIT_ACTION not found")
+	ErrConfigCommandFailed = Error("Config command failed")
+	ErrEmptyConfigCommandResponse = Error("Empty Config Command Response")
+	ErrDataplaneNotConnected = Error("Dataplane socket not connected")
 )
 
 type Conn struct {
@@ -216,4 +220,162 @@ func Interface(iface string) storeOpt {
 	return func(o *storeOptions) {
 		o.iface = iface
 	}
+}
+
+type DataplaneList struct {
+	Dataplanes []DataplaneData `json:"dataplanes"`
+}
+
+type DataplaneData struct {
+	Id	uint32 `json:"id"`
+	Timeout uint64 `json:"timeout"`
+	Local	bool	`json:"local"`
+	Connected bool	`json:"connected"`
+	Delpend bool `json:"delpend"`
+	Ctladdr string `json:"ctladdr"`
+	Clocktick  uint32 `json:"clocktick"`
+	Connects uint32 `json:"connects"`
+	Uuid string `json:"uuid"`
+	Sessionid string `json:"sessionid"`
+	Control string `json:"control"`
+	Interfaces []DataplaneIntf `json:"interfaces"`
+}
+
+type DataplaneIntf struct {
+	Index uint32 `json:"index"`
+	Name string `json:"name"`
+	State string `json:"state"`
+}
+
+type DataplaneConn struct {
+	mu sync.RWMutex
+	dpSock  *zmq.Sock
+
+	dp	*DataplaneData
+}
+
+
+func (c *Conn) GetDataplanes() ([]*DataplaneData, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	cmd := []byte("GETVPCONFIG")
+	err := c.configSocket.SendMessage([][]byte{cmd})
+	zmsg, err := c.configSocket.RecvMessage()
+
+	if err != nil {
+		return nil, err
+	}
+	if len(zmsg) == 0 {
+		return nil, ErrNoControllerResponse
+	}
+	if string(zmsg[0])  != "OK" {
+		return nil, ErrConfigCommandFailed
+	}
+	if len(zmsg) <= 1 {
+		return nil, ErrEmptyConfigCommandResponse
+	}
+
+	dplist := struct {
+		Dataplanes []*DataplaneData `json:"dataplanes"`
+	}{}
+	err = json.Unmarshal(zmsg[1], &dplist)
+	if err != nil {
+		return nil, err
+	}
+	return dplist.Dataplanes, nil
+}
+
+func DialDataplane(dp *DataplaneData) (*DataplaneConn, error) {
+	dpc := DataplaneConn {
+		dp: dp,
+	}
+	ep := dp.Control
+	if ep == "" {
+		ep = defaultDataplaneEndpoint
+	}
+	dpSock, err := zmq.NewReq(
+		ep,
+		zmq.SockSetLinger(0),
+		zmq.SockSetRcvtimeo(10000),
+		zmq.SockSetIpv6(1),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	dpc.dpSock = dpSock
+	runtime.SetFinalizer(&dpc, func(dpc *DataplaneConn) {
+		dpc.Close()
+	})
+	return &dpc, nil
+}
+
+func (dpc *DataplaneConn) Close() error {
+	dpc.mu.Lock()
+	defer dpc.mu.Unlock()
+
+	if dpc.dpSock == nil {
+		return nil
+	}
+	ep := dpc.dp.Control
+	if ep == "" {
+		ep = defaultDataplaneEndpoint
+	}
+	err := dpc.dpSock.Disconnect(ep)
+	if err != nil {
+		return err
+	}
+	dpc.dpSock.Destroy()
+	dpc.dpSock = nil
+	return nil
+}
+
+func (dpc *DataplaneConn) PBCmd(msgType string, msg, resp proto.Message) error {
+	dpc.mu.RLock()
+	defer dpc.mu.RUnlock()
+
+	if dpc.dpSock == nil {
+		return ErrDataplaneNotConnected
+	}
+
+	msgData, err := proto.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	de := DataplaneEnvelope.DataplaneEnvelope{
+		Type: proto.String(msgType),
+		Msg:  msgData,
+	}
+
+
+	pbout, err := proto.Marshal(&de)
+	if err != nil {
+		return err
+	}
+
+	outm := [][]byte{[]byte("protobuf"), pbout}
+
+	err = dpc.dpSock.SendMessage(outm)
+	if err != nil {
+		return err
+	}
+
+	zmsg, err := dpc.dpSock.RecvMessage()
+	if err != nil {
+		return err
+	}
+	rde := DataplaneEnvelope.DataplaneEnvelope {}
+	err = proto.Unmarshal(zmsg[0], &rde)
+	if err != nil {
+		return err
+	}
+	if resp != nil {
+		resp.Reset()
+		if rde.Msg != nil {
+			return proto.Unmarshal(rde.Msg, resp)
+		}
+	}
+	return nil
 }
